2830b04623d9ea87ce5cf81c053e979cc65c2537
Angelo <angeloparziale94@gmail.com>
2017-12-17 00:36:04 +0100

    nuovo file

---
+++ src/components/login/login.js
@@ -0,0 +1,18 @@
+import React from "React"
+
+export default class Login extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div style={{marginTop: "100px"}}>
+                <p>Sono la pagina di login</p>
+                <p>Sono la pagina di login</p>
+                <p>Sono la pagina di login</p>
+            </div>
+        )
+    }
+}
