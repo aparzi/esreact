2af7b11c8f073345a650f9f58433a6bdef567e8c
Angelo <angeloparziale94@gmail.com>
2017-12-16 17:39:49 +0100

    aggiunto il footer

--- src/components/mainLayout/footer/footer.js
+++ src/components/mainLayout/footer/footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import footerCss from "../../../css/mainLayout/footer/footer.css"
 
 export default class Footer extends React.Component {
 
@@ -8,8 +9,31 @@ export default class Footer extends React.Component {
 
     render() {
         return (
-            <div>
-                ecco Footer.
+            <div className="container-fluid footer">
+                <div className="row">
+                    <div className="col mr-auto">
+                        <p>Book Seller Company</p>
+                        <p>Via Alessandria, 4</p>
+                        <p>Toro, Campobasso, Italia</p>
+                        <p>AParzi@gmail.com</p>
+                    </div>
+                    <div className="col">
+                        <div className="pull-right">
+                            <a href="https://www.facebook.com/paolodellaguzzocom/" target="_blank">
+                                <i className="fa fa-facebook-official fa-2x"></i>
+                            </a>
+                            <a href="https://twitter.com/PaoloDellaguzzo" target="_blank">
+                                <i className="fa fa-twitter fa-2x"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/paolodellaguzzo/" target="_blank">
+                                <i className="fa fa-linkedin-square fa-2x"></i>
+                            </a>
+                            <a href="https://plus.google.com/u/0/+paolodellaguzzo" target="_blank">
+                                <i className="fa fa-google-plus-official fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
