f1ee42399bbbb53cba5e94ed3708c17feb47b936
Angelo <angeloparziale94@gmail.com>
2017-12-17 00:37:37 +0100

    fix more bug for navbar

--- src/components/mainLayout/header/header.js
+++ src/components/mainLayout/header/header.js
@@ -7,30 +7,23 @@ export default class Header extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-success">
+                <nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-success">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <a className="navbar-brand" href="#">
-                        <img src={Logo} className = "logo-image"/>
+                        <img src={Logo} className="logo-image"/>
                         <span>&nbsp;EsReact</span>
                     </a>
-                    <button
-                        className="navbar-toggler navbar-toggler-right"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarNavAltMarkup"
-                        aria-controls="navbarNavAltMarkup"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav mr-auto">
-                            <a className="nav-item nav-link" href="#">
+                            <a className="nav-item nav-link" href="#home-top">
                                 Home
                             </a>
-                            <a className="nav-item nav-link" href="#">What</a>
-                            <a className="nav-item nav-link" href="#">Who</a>
+                            <a className="nav-item nav-link" href="#what">Cosa facciamo</a>
+                            <a className="nav-item nav-link" href="#who">Chia siamo</a>
                         </div>
-                        <div className = "navbar-nav">
+                        <div className="navbar-nav">
                             <a className="nav-item nav-link" href="">Login</a>
                             <a className="nav-item nav-link" href="">Registrazione</a>
                         </div>
